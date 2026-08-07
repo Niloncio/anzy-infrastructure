af29c619dae3e948cf8776ef79a35a6acbfceac3
Ezequiel Nilo <ezequiel.nilo@latam.com>
2026-08-06 20:22:26 -0400

    "Add Terraform

---
+++ terraform/environments/intg/versions.tf
@@ -0,0 +1,17 @@
+############################################################
+# FILE: versions.tf
+#
+# ¿Quién interpreta este archivo?
+#
+# Terraform.
+#
+# ¿Qué responsabilidad tiene?
+#
+# Definir la configuración base del proyecto Terraform,
+# incluyendo la versión mínima de Terraform compatible.
+#
+############################################################
+
+terraform {
+  required_version = ">= 1.12.2"
+}ß
